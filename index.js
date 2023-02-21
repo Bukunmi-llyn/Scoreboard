23d8d96bb5097913e8440512abb4a05372f79906
Evelyn <ayansolaevelyn@gmail.com>
2023-02-21 05:44:25 +0100

    Return Guest name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let homeScoreBtnOne = document.getElementById("home-score-btn-1")
 let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
 
-function increaseHomeScoreOne(){
+function increaseHomeScoreOne() {
     homeScore += 1
     homeScoreEl.textContent = homeScore
 }
